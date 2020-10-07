a03600495d4773dc049562248cb3a34b5c3cd1b8
peterm790 <petermarsh790@gmail.com>
2020-10-07 15:35:57 +0200

    force resutls to float

--- jet1D/results_table.py
+++ jet1D/results_table.py
@@ -80,7 +80,7 @@ for index in dic:
     format(scipy.stats.pearsonr(list(seasonal[index].bottom),list(seasonal[index].pr))[1],'.2e'),
     ]
 
-
+df = df.iloc[:,:1].join(df.iloc[:,1:].astype('float'))
 #df.set_value('model','NOAA','NOAA 20CR')
 
-df.to_csv('../../JET_STREAM_OUT/jet1D/legranddtable_simplified.csv')
+df.to_csv('../../JET_STREAM_OUT/jet1D/legranddtable_simplified.csv',float_format='%.3f')
