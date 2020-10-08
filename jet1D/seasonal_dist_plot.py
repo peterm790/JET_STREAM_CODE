5d558b82a0bac699d81e622b5ea259406e23e009
peterm790 <petermarsh790@gmail.com>
2020-10-08 18:36:43 +0200

    updated seasonal plot

--- jet1D/seasonal_dist_plot.py
+++ jet1D/seasonal_dist_plot.py
@@ -8,7 +8,7 @@ import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-dic = pickle.load( open( "daily_jettrack.p", "rb" ) )
+dic = pickle.load( open( "../../JET_STREAM_OUT/jet1D/jettrack.p", "rb" ) )
 
 def is_winter(month):
     return (month == 6) | (month == 7) | (month == 8)
@@ -22,20 +22,29 @@ def is_autumn(month):
 def is_spring(month):
     return (month == 9) | (month == 10) | (month == 11)
 
-winter = pd.to_numeric(dic['NOAA'].where(is_winter(dic['NOAA'].months)).dropna().set_index('years').lat)
-summer = pd.to_numeric(dic['NOAA'].where(is_summer(dic['NOAA'].months)).dropna().set_index('years').lat)
-spring = pd.to_numeric(dic['NOAA'].where(is_spring(dic['NOAA'].months)).dropna().set_index('years').lat)
-autumn = pd.to_numeric(dic['NOAA'].where(is_autumn(dic['NOAA'].months)).dropna().set_index('years').lat)
+
+label = {'lat':'latitude',
+'width':'width',
+'ua':'velocity',
+'top':'Northern Extent',
+'bottom':'Southern Extent'}
 
 
-fig, ax1 = plt.subplots(figsize=(10,6))
-sns.kdeplot(list(winter),lw=3.5,color='b', vertical=True, label='Winter Width',ax=ax1)
-sns.kdeplot(list(summer),lw=3.5,color='r', vertical=True, label='Summer Width',ax=ax1)
-sns.kdeplot(list(spring),lw=3.5,color='g', vertical=True, label='spring Width',ax=ax1)
-sns.kdeplot(list(autumn),lw=3.5,color='y', vertical=True, label='Autumn Width',ax=ax1)
 
-ax1.set_xlabel("Kernel Density Estimate")
-ax1.set_ylabel("Latitude")
-plt.title('NOAA Jet Latitude Seasonal Distribution Shift', fontsize=16, ha='center')
-plt.savefig('lat_seasonality.png',dpi=100)
-plt.show()
+for metric in ['lat','width','ua','top','bottom']:
+    winter = pd.to_numeric(dic['NOAA'].where(is_winter(dic['NOAA'].months)).dropna().set_index('years')[metric])
+    summer = pd.to_numeric(dic['NOAA'].where(is_summer(dic['NOAA'].months)).dropna().set_index('years')[metric])
+    spring = pd.to_numeric(dic['NOAA'].where(is_spring(dic['NOAA'].months)).dropna().set_index('years')[metric])
+    autumn = pd.to_numeric(dic['NOAA'].where(is_autumn(dic['NOAA'].months)).dropna().set_index('years')[metric])
+    fig, ax1 = plt.subplots(figsize=(10,6))
+    sns.kdeplot(list(winter),lw=3.5,color='b', vertical=True, label='Winter Width',ax=ax1)
+    sns.kdeplot(list(summer),lw=3.5,color='r', vertical=True, label='Summer Width',ax=ax1)
+    sns.kdeplot(list(spring),lw=3.5,color='g', vertical=True, label='spring Width',ax=ax1)
+    sns.kdeplot(list(autumn),lw=3.5,color='y', vertical=True, label='Autumn Width',ax=ax1)
+    ax1.set_xlabel("Kernel Density Estimate")
+    ax1.set_ylabel(str(metric))
+    plt.title('NOAA Jet '+label[metric]+' Seasonal Distribution Shift', fontsize=16, ha='center')
+    plt.savefig('../../JET_STREAM_OUT/jet1D/seasonal_shift/png/'+str(metric)+'_seasonality.png',dpi=100)
+    plt.savefig('../../JET_STREAM_OUT/jet1D/seasonal_shift/pdf/'+str(metric)+'_seasonality.pdf')
+    plt.savefig('../../JET_STREAM_OUT/jet1D/seasonal_shift/svg/'+str(metric)+'_seasonality.svg',format='svg', dpi=1200)
+    plt.close()
